8aa6c9d68137f8f811a9796356dc275bc26723c7
oleeriksen <oe@easv.dk>
2021-02-22 22:39:16 +0100

    Update DetailActivity.kt

--- app/src/main/java/com/easv/oe/friends/GUI/DetailActivity.kt
+++ app/src/main/java/com/easv/oe/friends/GUI/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.easv.oe.friends.GUI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.easv.oe.friends.R
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -10,13 +11,19 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        val extras: Bundle = intent.extras!!
-        val name = extras.getString("name")
-        val phone = extras.getString("phone")
-        val favorite = extras.getBoolean("favorite")
-        tvName.text = name
-        tvPhone.text = phone
-        imgFavorite.setImageResource( if (favorite) R.drawable.ok else R.drawable.notok)
+        if (intent.extras != null) {
+            val extras: Bundle = intent.extras!!
+            val name = extras.getString("name")
+            val phone = extras.getString("phone")
+            val favorite = extras.getBoolean("favorite")
+            tvName.text = name
+            tvPhone.text = phone
+            imgFavorite.setImageResource(if (favorite) R.drawable.ok else R.drawable.notok)
+        }
+        else
+        {
+            Log.d("xyz", "system error: intent.extras for detailactivity is null!!!!")
+        }
     }
 
     fun onClickBack(view: View) { finish() }
