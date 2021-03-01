53b94cf3c10ea941c9c23cf03b951ec978be9552
oleeriksen <oe@easv.dk>
2021-03-01 19:06:59 +0100

    Update RecyclerAdapter.kt

--- app/src/main/java/com/easv/oe/friends/GUI/RecyclerAdapter.kt
+++ app/src/main/java/com/easv/oe/friends/GUI/RecyclerAdapter.kt
@@ -1,11 +1,8 @@
 package com.easv.oe.friends.GUI
 
 import android.content.Context
-<<<<<<< Updated upstream
 import android.graphics.Color
-=======
 import android.util.Log
->>>>>>> Stashed changes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
